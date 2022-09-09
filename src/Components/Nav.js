814ed8aeea90bc02d2ac5f78f71b51228f75f54b
Jhon Gomez <jhonjairogomez92@gmail.com>
2022-09-09 07:50:09 -0500

    Agregar servicios

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -9,7 +9,7 @@ export const Nav = ({ state, send }) => {
   return (
     <nav className='Nav'>
       <h1 className='Nav-logo'>Agenda un vuelo ✈</h1>
-      {!state.matches('initial') &&
+      {!state.matches('initial') && !state.matches('tickets') &&
         <button onClick={goToWelcome} className='Nav-cancel button-secondary'>Cancelar</button>
       }
     </nav>
